01e78ac7ffd7e09ba976f8508d83bdfa623fcdab
Minjae Seon <ddol0225@gmail.com>
2022-03-17 09:23:14 +0900

    Initial Commit

---
+++ ios/eric/example/example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  example
+//
+//  Created by Minjae Seon on 2022/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ server/eric/week1/week1.sql
@@ -0,0 +1,1 @@
+select * from mydata where idx = 1;
